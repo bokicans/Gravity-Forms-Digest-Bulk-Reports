999ffff575767f31ad80260d6ad4c53f8b1d96a8
Bojan <bokica@gmail.com>
2016-12-30 21:29:31 +0100

    - tag v0.3.3

--- gravityforms-digest/gravityforms-digest.php
+++ gravityforms-digest/gravityforms-digest.php
@@ -3,7 +3,7 @@
 		Plugin Name: Gravity Forms Digest Bulk Reports
 		Author: Path Interactive (orig. by Gennady Kovshenin)
 		Description: Generates bulk reports for submitted form entries and e-mails these as a digest to specific addresses
-		Version: 0.3.2
+		Version: 0.3.3
 		GitHub Plugin URI: https://github.com/bokicans/Gravity-Forms-Digest-Bulk-Reports
 	*/
 
